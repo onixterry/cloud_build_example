15e78be70112015db7f432793329cecdfdf594a5
Terry Chambers <terry@onixnet.com>
2021-08-19 17:54:01 +0000

    modified storage versioning parameter

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -1,4 +1,7 @@
 resource "google_storage_bucket" "bucket-for-state" {
   name        = "terry-terraform062021-cloudbuild"
   location    = "US"
+  versioning {
+          enabled = true
+        }
 }
