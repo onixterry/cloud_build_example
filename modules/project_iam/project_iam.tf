2b0535fa6a9d9015051220a418e7bc4c9acc533e
onixterry <terry@onixnet.com>
2021-09-01 10:23:22 -0400

    Adding additional role for terry@onixnet.com

--- modules/project_iam/project_iam.tf
+++ modules/project_iam/project_iam.tf
@@ -3,3 +3,9 @@ resource "google_project_iam_member" "project" {
   role    = "roles/editor"
   member  = "user:terry@onixnet.com"
 }
+
+resource "google_project_iam_member" "project" {
+  project = var.project_id
+  role    = "roles/compute.xpnAdmin"
+  member  = "user:terry@onixnet.com"
+}
