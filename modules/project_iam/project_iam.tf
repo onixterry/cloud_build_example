fccc5cf12b96b7aead9e89029688ea2b1ccdb777
onixterry <terry@onixnet.com>
2021-09-07 13:46:01 -0400

    Adding remaining roles and members to IAM policy

--- modules/project_iam/project_iam.tf
+++ modules/project_iam/project_iam.tf
@@ -4,6 +4,15 @@ resource "google_project_iam_member" "project" {
   member  = "user:terry@onixnet.com"
 }
 
+resource "google_project_iam_binding" "project_owners" {
+  project = var.project_id
+  role    = "roles/owner"
+
+  members = [
+    "user:terry@onixdev2.com"
+  ]
+}
+
 resource "google_project_iam_binding" "project_editors" {
   project = var.project_id
   role    = "roles/editor"
@@ -12,6 +21,32 @@ resource "google_project_iam_binding" "project_editors" {
     "user:terry@onixnet.com",
     "user:terry@searchdemo.onixnet.com",
     "serviceAccount:321230582999@cloudbuild.gserviceaccount.com"
+  ]
+}
 
+resource "google_project_iam_binding" "project_securityAdmin" {
+  project = var.project_id
+  role    = "roles/iam.securityAdmin"
+
+  members = [
+    "serviceAccount:321230582999@cloudbuild.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "project_roleAdmin" {
+  project = var.project_id
+  role    = "roles/iam.roleAdmin"
+
+  members = [
+    "serviceAccount:321230582999@cloudbuild.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "project_cloudBuildServiceAccount" {
+  project = var.project_id
+  role    = "roles/cloudbuild.builds.builder"
+
+  members = [
+    "serviceAccount:321230582999@cloudbuild.gserviceaccount.com"
   ]
 }
