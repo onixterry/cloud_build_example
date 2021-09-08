ce8ad1a0d576b48eddab67f30fe1921e35398c9c
onixterry <terry@onixnet.com>
2021-09-08 15:40:14 -0400

    Minor edit to permit build to run

--- modules/project_iam/project_iam.tf
+++ modules/project_iam/project_iam.tf
@@ -12,7 +12,6 @@ resource "google_project_iam_binding" "project_owners" {
     "user:terry@onixdev2.com"
   ]
 }
-
 resource "google_project_iam_binding" "project_editors" {
   project = var.project_id
   role    = "roles/editor"
