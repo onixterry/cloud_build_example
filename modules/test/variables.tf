d440fd5f7b018e66cdace88d63fe78c43582806f
onixterry <terry@onixnet.com>
2021-09-09 14:07:22 -0400

    Fixed editors

--- modules/test/variables.tf
+++ modules/test/variables.tf
@@ -8,7 +8,6 @@ variable "editor_list" {
   type        = list(string)
   description = "The list of Editors "
   default = ["terry@onixdev2.com",
-    "terry.chambers@gmail.com",
     "terry@onixnet.com"
       ]
 }
